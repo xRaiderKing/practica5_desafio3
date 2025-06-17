60ece10c67bbe38fc25dc019c003ed4840178cbe
Jesus Yael Padron Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-06-16 20:36:10 -0600

    Se realizan cambios en los derechos

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -14,7 +14,7 @@ return (
 </main>
 <footer style={{ padding: '10px', backgroundColor: '#f13710', color: '#fff',
 textAlign: 'center' }}>
-<p>Derechos Reservados© 2025 por Jesus Yael Padron Grimaldo </p>
+<p>Derechos Reservados© 2025 por Jesus Yael Padron Grimaldo - Junio</p>
 </footer>
 </div>
 );
