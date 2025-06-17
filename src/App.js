179bca941844b9c57f39b7799dfade2e19ba7130
Jesus Yael Padron Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-06-16 19:39:55 -0600

    Subir proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home() {
+return <h1>Bienvenidos a la Página de Inicio</h1>;
 }
-
+function About() {
+return <h2>Acerca de Nosotros</h2>;
+}
+function App() {
+return (
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Layout />}>
+<Route index element={<Home />} />
+<Route path="about" element={<About />} />
+</Route>
+</Routes>
+</BrowserRouter>
+);
+}
 export default App;
